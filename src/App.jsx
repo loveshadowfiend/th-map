60bd05d4fe2c3ed7b5e28922ed9461ec825aa3ee
loveshadowfiend <60311364+loveshadowfiend@users.noreply.github.com>
2024-04-19 22:33:58 +1000

    fix: removed old imports

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
-import "../node_modules/leaflet/dist/leaflet.css";
 import "../node_modules/reset-css/reset.css";
 import "./styles/style.css";
 import { Map } from "./components/Map";
 
 function App() {
     return (
-        <div className="map-container">
-            <Map />
+        <div>
+            <div className="map-container">
+                <Map />
+            </div>
         </div>
     );
 }
